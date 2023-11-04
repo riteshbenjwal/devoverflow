9f309b85038e760f514f23a26b93af6d098a41d4
riteshgeeky <ritesh.benjwal@geeky.dev>
2023-11-04 15:38:28 +0530

    add upvote downvote and collection ui, functionality

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,87 @@
+import NoResult from "@/components/shared/NoResult";
+import Filter from "@/components/shared/Filter";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { SearchParamsProps } from "@/types";
+// import Pagination from "@/components/shared/Pagination";
+import type { Metadata } from "next";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import QuestionCard from "@/components/card/QuestionCard";
+
+export const metadata: Metadata = {
+  title: "Collections | Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate withe developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
+
+export default async function Collection({ searchParams }: SearchParamsProps) {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const results = await getSavedQuestions({
+    clerkId: userId,
+    searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {results.questions.length > 0 ? (
+          results.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="Theres no saved question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. Your query could be the next big thing others learn from.
+          Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask Question"
+          />
+        )}
+      </div>
+
+      <div className="mt-10">
+        {/* <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={results.isNext}
+        /> */}
+      </div>
+    </>
+  );
+}
--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { getTimestamp } from "@/lib/utils";
 import ParseHTML from "./ParseHTML";
+import Votes from "./Votes";
 // import Votes from "./Votes";
 
 interface Props {
@@ -62,7 +63,7 @@ const AllAnswers = async ({
                 </div>
               </Link>
               <div className="flex justify-end">
-                {/* <Votes
+                <Votes
                   type="Answer"
                   itemId={JSON.stringify(answer._id)}
                   userId={JSON.stringify(userId)}
@@ -70,7 +71,7 @@ const AllAnswers = async ({
                   hasupVoted={answer.upvotes.includes(userId)}
                   downvotes={answer.downvotes.length}
                   hasdownVoted={answer.downvotes.includes(userId)}
-                /> */}
+                />
               </div>
             </div>
 
